7a64c995741c335a5879f0c3f1c8a0f3b65c8d12
Dyelson07 <dyelsoncarvalho14@gmail.com>
2026-09-13 04:21:30 -0300

    adiciona Material Icons ao projeto

--- Dashboard/src/main.js
+++ Dashboard/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './assets/styles/global.css'
 
+import '@material-design-icons/font/filled.css'
+
 createApp(App)
   .use(router)
   .mount('#app')
